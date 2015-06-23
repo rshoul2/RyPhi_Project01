0883af64c5f155bb736f46325f6461149910dbf5
Ryan Shoultz <rshoul2@gmail.com>
2015-06-22 21:29:38 -0500

    Our first program =)

---
+++ first.java
@@ -0,0 +1,12 @@
+public class first {
+	String phisting;
+
+	public first() {
+		this.phisting = "Hello, World!";
+	}
+
+	public static void main(String args[]) {
+		first anal = new first();
+		System.out.println(anal.phisting);
+	}
+}
